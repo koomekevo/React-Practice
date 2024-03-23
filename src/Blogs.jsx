0251450057bc4fc8a243e2886713f33d7e5be613
koomekevo <koome.kevo@gurucrafts.net>
2024-03-23 22:57:25 +0300

    Add useState

---
+++ src/Blogs.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "King Kunta", author: "Koome Kevo", id: 1 },
+    { title: "River Between", author: " Ngugi Theo", id: 2 },
+    { title: "Dungeon and Robbers", author: "Theo Walcott", id: 3 },
+  ]);
+
+  return <div></div>;
+};
+
+export default Blogs;
